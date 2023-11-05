76bf8d5868727b756992d54bb6a6c265d80e26ed
Johnny Wong <johnny.wong2324@gmail.com>
2023-11-05 13:39:36 -0500

    remove creds

--- life-style-go/src/lib/firebase/config.js
+++ life-style-go/src/lib/firebase/config.js
@@ -1,17 +1,9 @@
-const FIREBASE_API_KEY="AIzaSyDhs7tEcjYNa7bYBZs0EkSBcFJqV1zVWKE"
-const FIREBASE_AUTH_DOMAIN="lifestylego-api.firebaseapp.com"
-const FIREBASE_PROJECT_ID="lifestylego-api"
-const FIREBASE_STORAGE_BUCKET="lifestylego-api.appspot.com"
-const FIREBASE_MESSAGING_SENDER_ID="762749262626"
-const FIREBASE_APP_ID="1:762749262626:web:c472af47bcf6faab53637b"
-const FIREBASE_MEASUREMENT_ID="G-XGJS10YWDC"
-
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
